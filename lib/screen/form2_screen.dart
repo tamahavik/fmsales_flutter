249525168d9d53b3d64ee1d82bb0a6912cc5a87b
tamahavik <havik.priyatama.dev@gmail.com>
2023-01-25 22:25:20 +0700

    add form 2 ui

--- lib/screen/form2_screen.dart
+++ lib/screen/form2_screen.dart
@@ -11,8 +11,10 @@ class _Form2ScreenState extends State<Form2Screen> {
   final int _form = 2;
   String? _noHpActiveResult;
   String? _alamatValidResult;
+  String? _ketemuSiapaResult;
   final List<String> _noHpActiveDropdown = ["Ya", "Tidak"];
   final List<String> _alamatValidDropDown = ["Ya", "Tidak"];
+  final List<String> _ketemuSiapaDropDown = ["Ya", "Tidak"];
 
   @override
   Widget build(BuildContext context) {
@@ -192,6 +194,135 @@ class _Form2ScreenState extends State<Form2Screen> {
               ),
               SizedBox(
                 height: 20,
+              ),
+              Text('Ketemu Siapa'),
+              DropdownButton(
+                isExpanded: true,
+                hint: Text('- Silahkan Pilih -'),
+                value: _ketemuSiapaResult,
+                items: _ketemuSiapaDropDown.map((e) {
+                  return DropdownMenuItem(
+                    value: e,
+                    child: Text(e),
+                  );
+                }).toList(),
+                onChanged: (value) => {
+                  setState(() {
+                    _ketemuSiapaResult = value;
+                  })
+                },
+                icon: const Icon(Icons.keyboard_arrow_down),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              const Text('Tahun Motor'),
+              TextFormField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                cursorColor: Colors.black,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  focusColor: Colors.black,
+                  fillColor: Colors.black,
+                  hintText: 'Tahun Motor',
+                  hintStyle: TextStyle(
+                    color: Colors.grey[300],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              const Text('Merk Motor'),
+              TextFormField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                cursorColor: Colors.black,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  focusColor: Colors.black,
+                  fillColor: Colors.black,
+                  hintText: 'Merk Motor',
+                  hintStyle: TextStyle(
+                    color: Colors.grey[300],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              const Text('Tipe Motor'),
+              TextFormField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                cursorColor: Colors.black,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  focusColor: Colors.black,
+                  fillColor: Colors.black,
+                  hintText: 'Tipe Motor',
+                  hintStyle: TextStyle(
+                    color: Colors.grey[300],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              const Text('Keterangan'),
+              TextFormField(
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                cursorColor: Colors.black,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  focusColor: Colors.black,
+                  fillColor: Colors.black,
+                  hintText: 'Keterangan',
+                  hintStyle: TextStyle(
+                    color: Colors.grey[300],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 400,
+                padding: EdgeInsets.all(5),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                        color: Colors.amber,
+                        width: 2,
+                        style: BorderStyle.solid),
+                    top: BorderSide(
+                        color: Colors.amber,
+                        width: 2,
+                        style: BorderStyle.solid),
+                    left: BorderSide(
+                        color: Colors.amber,
+                        width: 2,
+                        style: BorderStyle.solid),
+                    right: BorderSide(
+                        color: Colors.amber,
+                        width: 2,
+                        style: BorderStyle.solid),
+                  ),
+                ),
+                child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.add,
+                    size: 300,
+                    color: Colors.amber,
+                  ),
+                ),
               ),
             ],
           ),
